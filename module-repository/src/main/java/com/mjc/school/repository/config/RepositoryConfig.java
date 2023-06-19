1098242078cd1b502743da9560cda5acd0773c39
Roman Gulevich <r.gulevich@syberry.com>
2023-06-19 15:49:03 +0300

    Commented RepositoryConfig and added enableAutoConfiguration class

--- module-repository/src/main/java/com/mjc/school/repository/config/RepositoryConfig.java
+++ module-repository/src/main/java/com/mjc/school/repository/config/RepositoryConfig.java
@@ -1,6 +1,7 @@
 package com.mjc.school.repository.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,58 +26,58 @@ import java.util.Properties;
 @EnableJpaRepositories("com.mjc.school.repository.impl")
 @PropertySource("classpath:application.properties")
 @EnableJpaAuditing
-//@EnableAutoConfiguration
+@EnableAutoConfiguration
 public class RepositoryConfig {
 
-    private static final String DATABASE_DRIVER = "spring.datasource.driver-class-name";
-    private static final String DATABASE_URL = "spring.datasource.url";
-    private static final String DATABASE_USERNAME = "spring.datasource.username";
-    private static final String DATABASE_PASSWORD = "spring.datasource.password";
-
-    private static final String HIBERNATE_DIALECT = "hibernate.dialect";
-    private static final String HIBERNATE_SHOWSQL = "hibernate.show_sql";
-    private static final String HIBERNATE_FORMATSQL = "hibernate.format_sql";
-    private static final String HIBERNATE_HBM2DDL = "hibernate.hbm2ddl.auto";
-
-    @Autowired
-    private Environment environment;
-
-    @Bean
-    public DataSource dataSource() {
-        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty(DATABASE_DRIVER)));
-        dataSource.setUrl(environment.getProperty(DATABASE_URL));
-        dataSource.setUsername(environment.getProperty(DATABASE_USERNAME));
-        dataSource.setPassword(environment.getProperty(DATABASE_PASSWORD));
-        return dataSource;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-
-        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
-        entityManagerFactory.setPackagesToScan("com.mjc.school.*");
-        entityManagerFactory.setDataSource(dataSource());
-        entityManagerFactory.setJpaProperties(hibernateProperties());
-        return entityManagerFactory;
-    }
-
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }
-
-    final Properties hibernateProperties() {
-        final Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty(HIBERNATE_HBM2DDL, environment.getProperty("spring.jpa.properties.hibernate.hbm2ddl.auto"));
-        hibernateProperties.setProperty(HIBERNATE_DIALECT, environment.getProperty("spring.jpa.properties.hibernate.dialect"));
-        hibernateProperties.setProperty(HIBERNATE_SHOWSQL, environment.getProperty("spring.jpa.show-sql"));
-        return hibernateProperties;
-    }
+//    private static final String DATABASE_DRIVER = "spring.datasource.driver-class-name";
+//    private static final String DATABASE_URL = "spring.datasource.url";
+//    private static final String DATABASE_USERNAME = "spring.datasource.username";
+//    private static final String DATABASE_PASSWORD = "spring.datasource.password";
+//
+//    private static final String HIBERNATE_DIALECT = "hibernate.dialect";
+//    private static final String HIBERNATE_SHOWSQL = "hibernate.show_sql";
+//    private static final String HIBERNATE_FORMATSQL = "hibernate.format_sql";
+//    private static final String HIBERNATE_HBM2DDL = "hibernate.hbm2ddl.auto";
+//
+//    @Autowired
+//    private Environment environment;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty(DATABASE_DRIVER)));
+//        dataSource.setUrl(environment.getProperty(DATABASE_URL));
+//        dataSource.setUsername(environment.getProperty(DATABASE_USERNAME));
+//        dataSource.setPassword(environment.getProperty(DATABASE_PASSWORD));
+//        return dataSource;
+//    }
+//
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//
+//        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+//        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
+//        entityManagerFactory.setPackagesToScan("com.mjc.school.*");
+//        entityManagerFactory.setDataSource(dataSource());
+//        entityManagerFactory.setJpaProperties(hibernateProperties());
+//        return entityManagerFactory;
+//    }
+//
+//
+//    @Bean
+//    public PlatformTransactionManager transactionManager() {
+//        JpaTransactionManager transactionManager = new JpaTransactionManager();
+//        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+//        return transactionManager;
+//    }
+//
+//    final Properties hibernateProperties() {
+//        final Properties hibernateProperties = new Properties();
+//        hibernateProperties.setProperty(HIBERNATE_HBM2DDL, environment.getProperty("spring.jpa.properties.hibernate.hbm2ddl.auto"));
+//        hibernateProperties.setProperty(HIBERNATE_DIALECT, environment.getProperty("spring.jpa.properties.hibernate.dialect"));
+//        hibernateProperties.setProperty(HIBERNATE_SHOWSQL, environment.getProperty("spring.jpa.show-sql"));
+//        return hibernateProperties;
+//    }
 
 }
